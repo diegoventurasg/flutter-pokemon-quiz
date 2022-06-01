6f6f4c565171752a7eccca32c35b9331993311e7
Diego Ventura <diegoventurasg@gmail.com>
2022-06-01 09:13:23 -0300

    add components

--- lib/app/modules/quiz/presentation/pages/quiz_page.dart
+++ lib/app/modules/quiz/presentation/pages/quiz_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_triple/flutter_triple.dart';
 
-import '../../domain/entities/question_entity.dart';
-import '../components/question/question_image.dart';
-import '../components/question/question_name.dart';
+import '../components/question/question.dart';
+import '../components/quiz_bar/quiz_bar.dart';
+import '../components/quiz_dialog/quiz_dialog.dart';
 import '../controllers/quiz_controller.dart';
-import '../stores/question_store.dart';
 
 class QuizPage extends StatefulWidget {
   const QuizPage({Key? key}) : super(key: key);
@@ -21,32 +19,23 @@ class _QuizPageState extends State<QuizPage> {
   @override
   void initState() {
     super.initState();
-    controller.questionStore.observer(
-      onError: (error) => print(error),
-    );
+    controller.init();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => controller.generateQuestion(),
-        child: const Icon(Icons.refresh),
-      ),
-      body: ScopedBuilder<QuestionStore, Exception, QuestionEntity>(
-        store: controller.questionStore,
-        onLoading: (_) => const Center(child: CircularProgressIndicator()),
-        onError: (_, exception) => const Center(child: Text('error')),
-        onState: (_, question) {
-          switch (question.type) {
-            case QuestionType.image:
-              return QuestionImage(question: question);
-            case QuestionType.name:
-              return QuestionName(question: question);
-            default:
-              return Container();
-          }
-        },
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              QuizBar(),
+              Question(),
+            ],
+          ),
+          QuizDialog(),
+        ],
       ),
     );
   }
