983c17bb4d6fc05a28ed96781950c43542cfbc0c
Diego Ventura <diegoventurasg@gmail.com>
2022-06-09 12:11:13 -0300

    add Dio and SharedPreferences binds

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'modules/quiz/quiz_guard.dart';
 import 'modules/quiz/quiz_module.dart';
 import 'modules/splash/splash_module.dart';
 
 class AppModule extends Module {
   @override
+  List<Bind<Object>> get binds => [
+        Bind.lazySingleton((i) => Dio()),
+        AsyncBind((i) => SharedPreferences.getInstance()),
+      ];
+
+  @override
   List<ModularRoute> get routes => [
         ModuleRoute('/', module: SplashModule()),
-        ModuleRoute('/quiz', module: QuizModule()),
+        ModuleRoute('/quiz', module: QuizModule(), guards: [QuizGuard()]),
       ];
 }
---
+++ lib/app/modules/quiz/quiz_guard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../app_module.dart';
+
+class QuizGuard extends RouteGuard {
+  @override
+  Future<bool> canActivate(String path, ModularRoute route) async {
+    await Modular.isModuleReady<AppModule>();
+    return true;
+  }
+}
